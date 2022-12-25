9067073e1eff6094c11eb0fe6c885f744e7a3217
PRANAV <120186818+pranav-tapdiya@users.noreply.github.com>
2022-12-25 22:18:56 +0530

    Create EX268.cpp

---
+++ EX268.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+      sort(nums.begin(),nums.end());
+      int i=0;
+      for(i=0;i<nums.size();i++)
+          if((nums[i] ^ i) !=0)break;          
+      return i;
+    }
+};
